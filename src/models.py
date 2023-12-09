5baad1c06a2000e50483dcf5f8ac5b139b17305f
KmiloPena <125616941+KmiloPena@users.noreply.github.com>
2023-12-09 05:50:26 +0000

    End

--- src/models.py
+++ src/models.py
@@ -7,23 +7,46 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    user_name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+class Characters(Base):
+    __tablename__ = 'characters'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    character_name = Column(String(250))
+    character_age = Column(String(250))
+
+class Planets(Base):
+    __tablename__ = 'planets'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    planet_name = Column(String(250))
+    planet_location = Column(String(250))
+
+
+class FavPlanet(Base):
+    __tablename__ = 'favPlanet'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    user_id = Column(Integer, ForeignKey('person.id'), ForeignKey('planet.id'))
+    person = relationship(User), relationship(Planets)
+    # planet_id = Column(Integer, ForeignKey('planet.id'))
+    # person = relationship(Planets)
+
+class FavCharacter(Base):
+    __tablename__ = 'favCharacter'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    user_id = Column(Integer, ForeignKey('person.id'), ForeignKey('character.id'))
+    person = relationship(User), relationship(Characters)
 
     def to_dict(self):
         return {}
